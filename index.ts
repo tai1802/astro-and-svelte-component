ef385e6c8ce0bda5e117f5a15e92f4c780ba4f68
Tai Nguyen <tai.nguyen@mgisolutions.com>
2023-09-06 18:08:00 +0700

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from '@components/MyComponent.astro';
+export { default as MyAstroComponent } from './src/MyAstroComponent.astro';
+export { default as MySvelteComponent } from './src/MySvelteComponent.svelte';
 
-export default MyComponent;
---
+++ src/MySvelteComponent.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let num: number = 0
+</script>
+
+<div>
+  Svelte Component: {num}
+</div>
+
